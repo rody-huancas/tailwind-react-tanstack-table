ce0e3027420b2e851a13a1df8fece2a2060d1ac3
Rody Huancas <rodyhuancas.04@gmail.com>
2024-02-22 15:26:42 -0500

    Add: list users

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { TanStackTable } from "./components/TanStackTable";
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="pt-4 min-h-screen bg-gray-900">
+      <TanStackTable />
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/components/TanStackTable.jsx
+++ src/components/TanStackTable.jsx
@@ -1,7 +1,99 @@
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { USER } from "../data";
+import { useState } from "react";
+
 export const TanStackTable = () => {
+  const columnHelper = createColumnHelper();
+
+  const columns = [
+    columnHelper.accessor("", {
+      id: "S.No",
+      cell: (info) => <span>{info.row.index + 1}</span>,
+      header: "S.No",
+    }),
+    columnHelper.accessor("profile", {
+      cell: (info) => (
+        <img
+          src={info?.getValue()}
+          alt="..."
+          className="rounded-full w-10 h-10 object-cover"
+        />
+      ),
+      header: "Profile",
+    }),
+    columnHelper.accessor("firstName", {
+      cell: (info) => <span>{info.getValue()}</span>,
+      header: "First Name",
+    }),
+    columnHelper.accessor("lastName", {
+      cell: (info) => <span>{info.getValue()}</span>,
+      header: "Last Name",
+    }),
+    columnHelper.accessor("age", {
+      cell: (info) => <span>{info.getValue()}</span>,
+      header: "Age",
+    }),
+    columnHelper.accessor("visits", {
+      cell: (info) => <span>{info.getValue()}</span>,
+      header: "Visits",
+    }),
+    columnHelper.accessor("progess", {
+      cell: (info) => <span>{info.getValue()}</span>,
+      header: "Progess",
+    }),
+  ];
+
+  const [data] = useState(() => [...USER]);
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
   return (
-    <div className="p-2 max-w-5xl mx-auto text-wrap fill-gray-400">
-      tanstack table
+    <div className="p-2 max-w-5xl mx-auto text-white fill-gray-400">
+      <table className="border border-gray-700 w-full text-left">
+        <thead className="bg-indigo-600">
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th key={header.id} className="capitalize px-3.5 py-2 ">
+                  {flexRender(
+                    header.column.columnDef.header,
+                    header.getContext()
+                  )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+
+        <tbody>
+          {table.getRowModel().rows.length
+            ? table.getRowModel().rows.map((row, i) => (
+                <tr key={row.id} className={`
+                ${i % 2 === 0 ? "bg-gray-900" : "bg-gray-800"}`}>
+                  {row.getVisibleCells().map((cell) => (
+                    <td key={cell.id} className="px-3.5 py-2">
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </td>
+                  ))}
+                </tr>
+              ))
+            : null}
+        </tbody>
+      </table>
+
+      {/* Pgination */}
     </div>
-  )
-}
+  );
+};
